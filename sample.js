3ad218b54ce3a196ea3b84b022c60b76f5c7b7c8
ayumi_arai <0araiayumi@gmail.com>
2019-07-22 13:19:05 +0900

    Add:methodsに追記、defalt_last_id修正

--- sample.js
+++ sample.js
@@ -6,7 +6,7 @@ new Vue({
         name: 'A山B郎',
         cource: 'Webエンジニアコース',
         acceptance_period: '2019年01月期',
-        default_last_id: 3,
+        id: 4,
         students: [
             {id: 1, name: '野呂浩良', cource: '機械学習コース', acceptance_period: '2019年01月期'},
             {id: 2, name: '富永修司', cource: 'Webエンジニアコース', acceptance_period: '2017年11月期'},
@@ -15,8 +15,15 @@ new Vue({
     },
     // methodsに、Vue.jsのインスタンスに使用させたいメソッドを記載する
     methods: {
-        addStudent: function () {
+        addStudent: function (event) {
+            this.students.push({
+                id: this.id++,
+                name: this.name,
+                cource: this.cource,
+                acceptance_period: this.acceptance_period
             // この function() { } の中にaddStudentメソッド
             // （入力欄に入力された値と、ID値（最後に登録されたID値+1の値）が新たに出現する機能）の処理を書く
+            })
         }
-}
+    }
+})
